72579b7f55061ea7e8b6d287dcaafecab73029ff
Ahmed Hani <ahmed.h.m112001@gmail.com>
2025-08-10 22:16:14 +0300

    Update1 Add the field and constructor to the member class

--- Member.cs
+++ Member.cs
@@ -8,5 +8,18 @@ namespace Library_System_Group2
 {
     public class Member
     {
+        
+        public int Id;
+        public string Name;
+        public int[] BorrowedBooks;
+        public int BorrowedCount;
+        public Member(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            BorrowedBooks = new int[3];
+            BorrowedCount = 0;
+        }
+
     }
 }
