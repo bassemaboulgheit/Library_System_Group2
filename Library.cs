cd9fb9bb9a2c1e4c8c0a9b951de957a996525d26
bassemaboulgheit <basem.abulgheit@gmail.com>
2025-08-11 17:43:56 +0300

    Update Library.cs

--- Library.cs
+++ Library.cs
@@ -2,52 +2,6 @@
 {
     public class Library
     {
-        
-
-        Book[] books;
-        int books_count;
-        Member[] members;
-        int members_count;
-        public Library()
-        {
-            books = new Book[20];
-            members = new Member[20];
-            books_count = 0;
-            members_count = 0;
-        }
-        
-
-       
-      //-------------FindBooks------------------//
-         public int FindBooks(int id)
-         {
-             for (int i = 0; i < books_count; i++)
-             {
-                 if (books[i].Id == id)
-                 {
-                     return i;
-                 }
-        
-             }
-             return -1;
-         }
-      //-------------listOfBook------------------//
-         public void listOfBook()
-         {
-             if (books_count != 0)
-             {
-                 for (int i = 0; i < books_count; i++)
-                 {
-                     Console.WriteLine(books[i]);
-                 }
-             }
-             else
-             {
-                 Console.WriteLine("there is no book in the Library");
-             }
-        
-         }
-
     }
 }
 
